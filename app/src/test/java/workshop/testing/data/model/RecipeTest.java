0187ebbf51701489763264fee6e9c69ede41ce5d
Thanawat Lukuan <thanawat_lukuan@cmu.ac.th>
2018-04-17 16:34:31 +0700

    commit mixed

--- app/src/test/java/workshop/testing/data/model/RecipeTest.java
+++ app/src/test/java/workshop/testing/data/model/RecipeTest.java
@@ -31,5 +31,24 @@ public class RecipeTest {
         assertEquals("Water",recipe.title);
         assertEquals("Put glass under tap. Open tap. Close tap. Drink.",recipe.description);
     }
+    @Test
+    public void mixed(){
+        //Arrange
+        InputStream inputStream = RecipeTest.class.getResourceAsStream("/recipes/mixed.txt");
+        //Act
+        Recipe recipe = Recipe.readFromStream(inputStream);
+        //Assert
+        assertEquals("punch", recipe.id);
+        assertEquals("Punch",recipe.title);
+        assertEquals(
+                "Juice of 3 lemons\n" +
+                "1 orange\n" +
+                "1 pint grape juice\n" +
+                "1 cup sugar\n" +
+                "1 cup water\n" +
+                "1 pine apple juice\n" +
+                "Mix all together and strain. Add large piece of ice.",recipe.description);
+
+    }
 
 }
